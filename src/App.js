5d11012dd411c0120ad627ed10240016401df224
chaya <chayapunnam13@gmail.com>
2026-05-31 20:27:38 +0530

    Initial React Study App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import bg from "./assets/bg.jpg";
+import logo from "./assets/logo.png";
 
 function App() {
+  const links = {
+    C: "https://www.w3schools.com/c/",
+    "C++": "https://www.w3schools.com/cpp/",
+    Java: "https://www.w3schools.com/java/",
+    Python: "https://www.w3schools.com/python/",
+    SQL: "https://www.w3schools.com/sql/",
+    React: "https://react.dev/",
+  };
+
+  const openLink = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <header className="header">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="login-box">
+          <h3>Login</h3>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+        </div>
       </header>
+
+      <main className="main">
+        <h1>Study App</h1>
+        <div className="welcome-box">
+          <p>Welcome to Study App.</p>
+          <p>Here you can learn multiple programming languages.</p>
+          <p>Click on any language button below to explore.</p>
+          <p>Happy Learning!</p>
+        </div>
+
+        <div className="button-grid">
+          {Object.keys(links).map((lang) => (
+            <button key={lang} onClick={() => openLink(links[lang])}>
+              {lang}
+            </button>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
